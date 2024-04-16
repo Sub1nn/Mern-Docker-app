042fae7da45f9c374b434c404820bba570a7b014
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-04-16 23:42:46 +0300

    Simple App frontEnd part completed

---
+++ client/src/App.jsx
@@ -0,0 +1,38 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import "./App.css"
+
+function App() {
+  const [notes, setNotes] = useState(null)
+
+  useEffect(() => {
+    axios.get("http://localhost:4000/api/notes/add").then((res) => {
+      console.log(res?.data)
+      setNotes(res?.data?.data)
+    })
+  }, [])
+
+  return (
+    <>
+      <div className="App">
+        <h1>Notes</h1>
+        {notes ? (
+          <ul>
+            {notes?.map((note, index) => {
+              return (
+                <li key={index}>
+                  <h2>{note.title}</h2>
+                  <p>{note.content}</p>
+                </li>
+              )
+            })}
+          </ul>
+        ) : (
+          <p>Loading....</p>
+        )}
+      </div>
+    </>
+  )
+}
+
+export default App
